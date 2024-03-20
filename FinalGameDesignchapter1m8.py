a26433382825ff7ad2a42a8f9f00946c15ea5ae5
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-20 18:02:12 -0500

    FinalGameDesignchapter1m8.py

--- FinalGameDesignchapter1m8.py
+++ FinalGameDesignchapter1m8.py
@@ -2,15 +2,19 @@ from Gvars import *
 def pick_name():
     import random
     name = input("Choose who you want. 1. Cleo, 2. TT, 3. Stoney, 4. Frankie")
-    
-    
+
+
     while name not in ["1","2","3","4"]:
         print("This is not a option. Game Over")
         name = input("Choose who you want. 1. Cleo, 2. TT, 3. Stoney, 4. Frankie")
-    
+
     if name == ("2"):
         name = "TT"
         print("Let's go to the agency and get my son so we can leave out the country.")
+    elif name == ("Game over"):
+        print("Game over")
+    elif name == ("0"):
+        print("Game over")
     elif name == "3":
         name = ("Stoney")
         print("Let go to the hood and ask one of them men to give us some money.")
@@ -22,8 +26,13 @@ def pick_name():
         print("Let's go rob Bank Federal Downtown.")
     else:
         print("That is not a option. Please choose again.")
-    for num in names:
-        print (num) 
+
+    if name == "TT":
+        print("Game over")
+        exit()
+    else:
+        for num in names:
+            print (num)
                     #names.remove("Stoney")
                     #names.remove("Frankie")
                     #names.remove("Cleo")
